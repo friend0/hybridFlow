2ad0f6131140cd25531c625bb385d205005406e3
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2016-02-24 23:17:59 -0800

    Added BLE advertisement to Simblee, updates to FSM.

--- acousticFlowSimblee/acousticFlowSimblee.ino
+++ acousticFlowSimblee/acousticFlowSimblee.ino
@@ -24,7 +24,6 @@ bbStates state = DEF;
 #define MAX_DATA_SIZE 10
 uint8_t testData[MAX_DATA_SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-
 ////////////////////
 // Internal State //
 ////////////////////
